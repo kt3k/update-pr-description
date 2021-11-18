fa47a8b10e79a389854fc5008c3921d492af249c
Denis Tokarev <denis.t@canva.com>
2021-11-18 16:20:57 +1100

    Minor updates

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const run = async () => {
   };
 
   const octokit = github.getOctokit(githubToken);
-  core.info(`Looking up a pull request with source branch="${sourceBranch || '<not found>'}" and base branch="${baseBranch || '<not specified>'}"`);
+  core.info(`Looking up a pull request with a source branch "${sourceBranch || '<not found>'}" and a base branch "${baseBranch || '<not specified>'}"`);
 
   const branchHead = `${credentials.owner}:${sourceBranch}`;
   const { data: pulls } = await octokit.rest.pulls.list({
@@ -24,35 +24,40 @@ const run = async () => {
   });
 
   if (pulls.length === 0) {
-    throw new Error(`No pull request found for source="${source || '<not found>'}" base=${baseBranch || '<not specified>'}`);
+    throw new Error(`No pull request found for a source branch "${sourceBranch || '<not found>'}" and a base branch "${baseBranch || '<not specified>'}"`);
   }
 
   const pullRequest = pulls.find((p) => p.state === 'open');
   if (pullRequest == null) {
-    throw new Error(`No open pull request found for source="${source || '<not found>'}" base=${baseBranch || '<not specified>'}`);
+    throw new Error(`No open pull requests found for a source branch "${sourceBranch || '<not found>'}" and a base branch "${baseBranch || '<not specified>'}"`);
   }
 
   const { number: pullNumber } = pullRequest;
-  core.info(`Found pull request #${pullNumber}`);
+  core.info(`Pull request #${pullNumber} has been found for  a source branch "${sourceBranch || '<not found>'}" and a base branch "${baseBranch || '<not specified>'}"`);
 
   const params = {
     ...credentials,
     pull_number: pullNumber,
   };
 
+  if (prTitle) {
+    core.info(`Pull request #${pullNumber}'s title will be set to "${prTitle}"`);
+    params.title = prTitle;
+  }
+
   if (prBody) {
-    core.info(`Updating with body="${prBody}"`);
+    core.info(`Pull request #${pullNumber}'s body will be set to "${prBody}"`);
     params.body = prBody;
   }
 
-  if (prTitle) {
-    core.info(`Updating with title=#${prTitle}`);
+  if (baseBranch) {
+    core.info(`Pull request #${pullNumber}'s base branch will be set to "${baseBranch}"`);
     params.title = prTitle;
   }
 
   const url = `/repos/${credentials.owner}/${credentials.repo}/pulls/${pullNumber}`;
 
-  core.info(`Making PATCH request to "${url}" with params ${JSON.stringify(params)}`);
+  core.info(`Making a PATCH request to "${url}" with params "${JSON.stringify(params)}"`);
   await octokit.request(`PATCH ${url}`, params);
 };
 
@@ -61,5 +66,6 @@ run()
     core.info('Done.');
   })
   .catch((e) => {
+    core.error('Cannot update the pull request.');
     core.setFailed(e.stack || e.message);
   });
